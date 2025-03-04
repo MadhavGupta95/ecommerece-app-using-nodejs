c023eb0bd3a56cc63700eb594c1913796b3fa97d
Madhav Gupta <guptamadhav2005@gmail.com>
2025-03-05 00:01:21 +0530

    login signup and main website work is finish, tomorrow i will figure out how to make this store api local and make dashboard to add and delete items

--- routes/pages.js
+++ routes/pages.js
@@ -1,11 +1,20 @@
-import path from "node:path"
-import express, {Router} from 'express'
+import path from "node:path";
+import express, { Router } from "express";
 
-const router = express.Router()
+const router = express.Router();
 
-router.get("/", (req, res)=>{
-    const filePath = path.join(path.resolve(), "pages" , "website.html")
+router.get("/", (req, res) => {
+  const filePath = path.join(path.resolve(), "pages", "website.html");
+  return res.sendFile(filePath);
+});
+
+router.get("/signup", (req, res) => {
+    const filePath = path.join(path.resolve(), "pages", "signup.html");
+    return res.sendFile(filePath);
+  });
+
+router.get('/login', (req, res)=>{
+    const filePath = path.join(path.resolve(), "pages", "login.html")
     return res.sendFile(filePath)
 })
-
-export default router
+export default router;
